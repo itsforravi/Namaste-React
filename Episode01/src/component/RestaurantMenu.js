62765f7640ff75d2b8a73641850a8142f4861c30
itsforravi <itsforravi@gmail.com>
2023-09-06 09:43:51 +0530

    react

--- Episode01/src/component/RestaurantMenu.js
+++ Episode01/src/component/RestaurantMenu.js
@@ -1,25 +1,18 @@
  import { useState,useEffect } from "react";
  import Shimmer from "./shimmer";
  import { useParams } from "react-router-dom";
- import { MENU_API } from "../utilis/contants";
+ 
 
  
  const RestaurantMenu=()=>{
  
-const [resInfo,setResInfo]=useState(null);
-const {resId}=useParams();
 
-    useEffect(()=>{
-  fetchMenu();
-    });
-    const fetchMenu=async ()=>{
-        const data=await fetch(
-            MENU_API+resId
-        );
-        const json=await data.json();
-        console.log(json);
-        setResInfo(json.data);
-    };
+const {resId}=useParams();
+const resInfo=useRestaurantMenu(resId);
+  
+
+
+
     if( resInfo===null)  return  <Shimmer/> ;
 
 const {name,cuisines,cloudinaryImageId,costForTwo,avgRating}=resInfo?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants[0]?.info;
