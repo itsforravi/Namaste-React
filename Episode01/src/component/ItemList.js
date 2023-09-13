dd600a0ce19e63c0d5b741f0f636ae4eb8cbcc93
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-13 10:44:11 +0530

    react

--- Episode01/src/component/ItemList.js
+++ Episode01/src/component/ItemList.js
@@ -4,16 +4,15 @@ const ItemList=(items)=>{
 
 console.log(items);
 
-return ( <div>
+return ( 
    <div>
     {items.map((item)=>
     (<div key={item.card.info.id} 
         className="p-2 m-2  border-gray-200 
     border-b-2 text-left flex justify-between">
 
-
-
-<div className="py-2">
+<div className="w-9/122">
+         <div className="py-2">
     <span>{item.card.info.name}</span>
     <span>-₹{item.card.info.price 
     ?item.card.info.price
@@ -21,15 +20,22 @@ return ( <div>
     
     </div>
     <p className="text-xs">{item.card.info.description}</p>
+    </div>
+    <div className="w-3/12 p-4">
+        <img src={CDN_URL+item.card.info.imageId} className="w-full"/>
+    </div>
+</div>
+ 
 
-    </div>
-    
-    ))}
+
+   
+   
+   ))}
+   
    </div>
-    </div>
 
 );
-}
+};
 
 
 export default ItemList;
