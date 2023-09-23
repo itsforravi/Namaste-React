4a61316b50de00d3afaa496374da8f1d63442d9e
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-23 14:47:10 +0530

    react

--- Episode01/src/component/__tests__/Contact.test.js
+++ Episode01/src/component/__tests__/Contact.test.js
@@ -1,16 +1,20 @@
 import {render,screen} from "@testing-library/react";
 import Contact from "../Contact";
 import "@teating-library/jest-dom";
+describe("Contact Us Page Test Case" ,()=>{
 
-test("Should load contact us component ",()=>{
-    render(<Contact/>);
-    const heading=screen.getByRole("heading");
+    test("Should load contact us component ",()=>{
+        render(<Contact/>);
+        const heading=screen.getByRole("heading");
+    
+        expect(heading).toBeInTheDocument();
+    });
+    test("Should load button inside Contact component ",()=>{
+        render(<Contact/>);
+        const button=screen.getByText("Submit");
+    
+        expect(button).toBeInTheDocument();
+    });
 
-    expect(heading).toBeInTheDocument();
-});
-test("Should load button inside Contact component ",()=>{
-    render(<Contact/>);
-    const button=screen.getByText("Submit");
+})
 
-    expect(button).toBeInTheDocument();
-});
