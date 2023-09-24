08d63f5837245cda12443bb029e81c144af0363b
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-24 16:05:53 +0530

    react

--- Episode01/src/component/__tests__/Search.test.js
+++ Episode01/src/component/__tests__/Search.test.js
@@ -2,6 +2,8 @@ import Body from "../Body"
 import {render} from "@testing-library/react";
 import MOCK_DATA from "../../component/mocks/mockResListData.json"
 import { act } from "react-dom/test-utils";
+import { BrowserRouter } from "react-router-dom";
+
 
 globalThis.fetch=jest.fn(()=>{
     return Promise.resolve({
@@ -14,6 +16,10 @@ globalThis.fetch=jest.fn(()=>{
 
 it("Should render the body with search",async()=>{
   
-   await act(async()=>render(<Body/>));
+   await act(async()=>render(
+   <BrowserRouter>
+   <Body/>
+   </BrowserRouter>
+   ));
 
 });
