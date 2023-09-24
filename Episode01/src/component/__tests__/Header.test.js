3d21cc5dc826490c3cfb3c5a71855503c299d101
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-24 13:48:53 +0530

    react

--- Episode01/src/component/__tests__/Header.test.js
+++ Episode01/src/component/__tests__/Header.test.js
@@ -14,8 +14,26 @@ render(
 </Provider>
 </BrowserRouter>
 );
-const loginButton=screen.getByRole("button");
+const loginButton=screen.getByRole("button",{name:"Login"});
+// const loginButton=screen.getByText("Login")
+
 expect(loginButton).toBeTheDocument();
+
+
+})
+ItemList("Should render Header Component with a Cart items 0",()=>{
+
+render(
+    <BrowserRouter>
+<Provider store={appStore}>
+<Header/>
+</Provider>
+</BrowserRouter>
+);
+const cartItems=screen.getByText("Cart-(0 items)");
+// const loginButton=screen.getByText("Login")
+
+expect(cartItems).toBeTheDocument();
 
 
 })
