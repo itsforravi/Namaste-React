02ab1f62b1e1768a43bdc223550c68b14fd13a29
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-24 12:51:34 +0530

    react

--- Episode01/src/component/__tests__/Header.test.js
+++ Episode01/src/component/__tests__/Header.test.js
@@ -1,8 +1,18 @@
+import { render } from "react-dom";
+import { Provider } from "react-redux";
 import Header from "../Header"
+import appStore from "../../utilis/appStore";
+import { BrowserRouter } from "react-router-dom";
 
 
 ItemList("Should Header Component with a login button",()=>{
 
-render(<Header/>);
+render(
+    <BrowserRouter>
+<Provider store={appStore}>
+<Header/>
+</Provider>
+</BrowserRouter>
+);
 
 })
