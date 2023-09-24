97e47edfd6dac805bf74c0e5d5d4188682a20880
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-24 13:57:15 +0530

    react

--- Episode01/src/component/__tests__/Header.test.js
+++ Episode01/src/component/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render } from "react-dom";
+import { render ,screen,fireEvent} from "react-dom";
 import { Provider } from "react-redux";
 import Header from "../Header"
 import appStore from "../../utilis/appStore";
@@ -21,6 +21,8 @@ expect(loginButton).toBeTheDocument();
 
 
 })
+
+
 ItemList("Should render Header Component with a Cart items 0",()=>{
 
 render(
@@ -31,6 +33,42 @@ render(
 </BrowserRouter>
 );
 const cartItems=screen.getByText("Cart-(0 items)");
+// const loginButton=screen.getByText("Login")
+
+expect(cartItems).toBeTheDocument();
+
+
+})
+
+ItemList("Should change login button to logout on click  ",()=>{
+
+render(
+    <BrowserRouter>
+<Provider store={appStore}>
+<Header/>
+</Provider>
+</BrowserRouter>
+);
+const loginButton=screen.getByText("button",{name:"Login"});
+
+fireEvent.click(loginButton);
+
+const logoutButton=screen.getByText("button",{name:"Logout"});
+
+expect(logoutButton).toBeTheDocument();
+
+
+})
+ItemList("Should render Header Component with a Cart items ",()=>{
+
+render(
+    <BrowserRouter>
+<Provider store={appStore}>
+<Header/>
+</Provider>
+</BrowserRouter>
+);
+const cartItems=screen.getByText("/Cart/");
 // const loginButton=screen.getByText("Login")
 
 expect(cartItems).toBeTheDocument();
