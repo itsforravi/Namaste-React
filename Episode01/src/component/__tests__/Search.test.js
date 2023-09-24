d0ee9c15a7c3cff6f360a19ddc9dae74712578ab
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-24 15:44:53 +0530

    react

--- Episode01/src/component/__tests__/Search.test.js
+++ Episode01/src/component/__tests__/Search.test.js
@@ -1,2 +1,18 @@
+import Body from "../Body"
+import {render} from "@testing-library/react";
+import MOCK_DATA from "../../component/mocks/mockResListData.json"
+
+globalThis.fetch=jest.fn(()=>{
+    return Promise.resolve({
+        json:()=>{
+            return Promise.resolve(MOCK_DATA);
+        }
+    })
+})
 
 
+it("Should render the body component with search button",()=>{
+  
+    render(<Body/>);
+
+});
