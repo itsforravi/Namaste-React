5eedf6c7ad7db39232e0e0f9146f1d997ef8583a
Ravi Kumar Gupt <87171097+therealravi8808@users.noreply.github.com>
2023-09-25 15:50:16 +0530

    react

--- Episode01/src/component/__tests__/Cart.test.js
+++ Episode01/src/component/__tests__/Cart.test.js
@@ -1,18 +1,26 @@
-import { render } from "@testing-library/react"
+import { fireEvent, render ,screen} from "@testing-library/react"
 import { act } from "react-dom/test-utils";
 import RestaurantMenu from "../RestaurantMenu";
-
+import MOCK_DATA_NAME from "../../component/mocks/mockResMenu.json"
+import { Provider } from "react-redux";
+import appStore from "../../utilis/appStore";
 
 global.fetch=jest.fn(()=>
  Promise.resolve({
-        json:()=> Promise.resolve(MOCK_DATA)
+        json:()=> Promise.resolve(MOCK_DATA_NAME)
         })
 )
 
 it("should load Restaurant Menu Component ",async()=>{
 
-await act (async()=>render(<RestaurantMenu/>
+await act (async()=>render(
+<Provider store={appStore} >
+<RestaurantMenu/>
+</Provider>));
 
-    ));
+const accordionHeader=screen.getByText("Biryani(5)");
+fireEvent.click(accordionHeader);
+    
+    expect(screen.getAllByAltTextId("foodItems").length).toBe(5);
 
 })
